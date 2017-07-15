02d012b1edaa9cb64772c9910cccb5761fc82c69
sumanbhushal <sumanbhushal@gmail.com>
2017-07-15 09:15:15 +0200

    loading thumb images into setting

--- Optical Floater Remover/Setting.xaml.cs
+++ Optical Floater Remover/Setting.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Optical_Floater_Remover.controller;
+using Optical_Floater_Remover.model;
 
 namespace Optical_Floater_Remover
 {
@@ -19,9 +21,28 @@ namespace Optical_Floater_Remover
     /// </summary>
     public partial class Setting : Window
     {
+        private readonly List<FloaterImage> fImages = new List<FloaterImage>();
+        private string setImageName;
+        private double setNewOpacityValue;
+        private readonly BaseController _floaterImagerController = new BaseController();
         public Setting()
         {
             InitializeComponent();
+            PopulateListViewWithImage();
+        }
+
+        private void PopulateListViewWithImage()
+        {
+            fImages.Add(new FloaterImage() { ImageName = "Turbulence-GIF.gif", ImageSourceLocation = "pack://application:,,,/img/Turbulence-GIF-small.gif" });
+            fImages.Add(new FloaterImage() { ImageName = "Turbulence-GIF.gif", ImageSourceLocation = "pack://application:,,,/img/Turbulence-GIF-small.gif" });
+            fImages.Add(new FloaterImage() { ImageName = "Turbulence-GIF.gif", ImageSourceLocation = "pack://application:,,,/img/Turbulence-GIF-small.gif" });
+            fImages.Add(new FloaterImage() { ImageName = "Turbulence-GIF.gif", ImageSourceLocation = "pack://application:,,,/img/Turbulence-GIF-small.gif" });
+            //fImages.Add(new FloaterImage() { ImageName = "black_dots.png", ImageSourceLocation = "pack://application:,,,/Include/black_dots.png" });
+            LboxItem.ItemsSource = fImages;
+        }
+        private void LboxItemSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ Optical Floater Remover/model/FloaterImage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Optical_Floater_Remover.model
+{
+    class FloaterImage
+    {
+        public int Id { get; set; }
+        public string ImageName { get; set; }
+        public string ImageSourceLocation { get; set; }
+    }
+}
